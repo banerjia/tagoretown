c9b836773ec41cc52fc42e1ebc229ed51aa0694b
Abhishek Banerji <webdesign.banerji@gmail.com>
2016-01-08 20:23:33 -0500

    Bootstrap Updates and Django Attr Plugin

--- tagoretown/settings.py
+++ tagoretown/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #   'pipeline',
+    'widget_tweaks',
     'oam.apps.OAMConfig',
 ]
 
